dbf0e3714544daabe13854112f6d0811f74b9dbe
adm <adm@labsoft.local>
2016-01-22 18:50:25 -0200

    Att. Aula 03

--- Aulas/Aula 03.R
+++ Aulas/Aula 03.R
@@ -1,10 +1,17 @@
 require(dplyr)
 
 #Carregando o banco:
-data(pnud_muni, package ='abjutils')
+data(pnud_muni, package ='abjutils') #criando um data frame de pnud_muni
 pnud_muni <- tbl_df(pnud_muni)
 pnud_muni #se eu imprimir no console, n contará todas as linhas
 
 #-------------------------SELECT:
+  
 
+#### Posso selecionar:
+# por indice (nao recomendavel!)
+# especificando nomes
+# intervalos e funcoes auxiliares (começa por, tem, etc)
+
+#selecioando algumas colunas
 pnud_muni %>% select(uf, municipio, ano, gini, idhm)
