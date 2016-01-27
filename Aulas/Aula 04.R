4457ca08e9d44c2ab39a3e58355a03355dd56dc6
adm <adm@labsoft.local>
2016-01-27 19:57:26 -0200

    GG Aula 04

--- Aulas/Aula 04.R
+++ Aulas/Aula 04.R
@@ -1,4 +1,5 @@
 #install.packages("ggplot2")
+#install.packages("maps")
 #Pastel on Dark
 library(ggplot2)
 
@@ -49,3 +50,72 @@ ggplot(data = diamonds, aes(y = price, x = carat, color = cut)) +
 
 library(dplyr)
 library(tidyr)
+
+view(economics)
+
+ggplot(economics, aes(x = date, y = unemploy)) + geom_line()
+ggplot(economics, aes(x = date, y = uempmed)) + geom_line()
+
+#escalas dos dois diferentes, então:
+#padronizo os 2 e deixo na msma escala:
+
+economics %>%
+  mutate(unemploy = ((unemploy - min(unemploy))/(max(unemploy) - min(unemploy))),
+         uempmed = ((uempmed - min(uempmed))/(max(uempmed) - min(uempmed)))
+  ) %>%
+  ggplot(aes(date, unemploy)) + 
+  geom_line() +
+  geom_line(aes(y = uempmed), colour = "blue")
+
+
+#padronizandos de outro jeito (melhor q o primeiro):
+economics %>%
+  select(date, unemploy, uempmed) %>%
+  gather(indice, valor, -date) %>%
+  group_by(indice) %>% 
+  mutate(valor_padronizado = ((valor - min(valor))/(max(valor) - min(valor)))) %>%
+  ggplot(aes(x = date, y = valor_padronizado, color = indice)) + geom_line() +
+  scale_color_manual("Índice", values = c("grey", "sky blue"),
+                     labels = c("Desemprego", "Tempo Desemprego")
+                     ) +
+  labs(x = "Data", y = "Valor")
+
+
+#Colocar uma reta de regressão em um gráfico de dispersão:
+ggplot(data = diamonds, aes(y = price, x = carat)) + 
+  geom_point() + 
+  geom_abline(intercept = -2256.361, slope = 7756.426, color = "light grey", 
+              linetype = "dashed", size = 1)
+
+#retorna slope (coef angular) e intercept
+coef(lm(price ~ carat, data = diamonds))
+
+#Outro jeito de aplicar:
+ggplot(data = diamonds %>% sample_n(1000), aes(y = price, x = carat)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") #ele tbm traz intervalo de confiança
+#com smooth, paraca cada modelo de cut ele vai ajeitar:
+ggplot(data = diamonds %>% sample_n(1000), aes(y = price, x = carat, color = cut)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") +
+  facet_wrap(~cut)
+
+
+####Gráfico de barrar: Quantidade de veículos por classe
+diamonds %>%
+  group_by(cut) %>%
+  summarise(n = n()) %>%
+  ggplot(aes(x = cut, y = n)) +
+  geom_bar(stat = "identity")
+
+# Mais fácil:
+ggplot(diamonds, aes(x = cut)) +
+  geom_bar()
+
+####Mapa das ocorrências:
+mapa <- map_data("world")
+
+quakes %>%
+  ggplot(aes(x=long, y=lat)) +
+  geom_density2d(aes(size=stations)) +
+  geom_map(aes(map_id=region), map=mapa, data=mapa)
