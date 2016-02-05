f0940b463ab7bfa98f63733c96be778ac5e84c9e
adm <adm@labsoft.local>
2016-02-05 19:42:04 -0200

    Aula 08 UP!

---
+++ Aulas/Aula 08.R
@@ -0,0 +1,13 @@
+#http://shiny.rstudio.com/tutorial/
+#TUTORIAL SHINY - VER /\
+
+#install.packages('shiny')
+library(shiny)
+
+shiny::runGitHub('abjur/vistemplate',
+                 subdir='exemplo_01_helloworld',
+                 display.mode='showcase')
+
+
+
+### LISTA DE WIDGETS = http://shiny.rstudio.com/gallery/widget-gallery.html
---
+++ Aulas/Aula08Exe1/ui.R
@@ -0,0 +1,40 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Exercicio!"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    wellPanel(
+      dateInput(inputId = "date", "Data:"),
+      checkboxGroupInput(inputId = "valores", "Valores:", c("vl1" = "Valor 1",
+                                                  "vl2" = "valor 2",
+                                                  "vl3" = "valor 3"
+                                                  )),
+      textInput(inputId = "txtTexto", "Caption:", "Data Summary"),
+      numericInput('n', label = 'N', value = 3)
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+       #plotOutput("distPlot")
+      fluidRow(
+        tableOutput("id_tab")
+      ),
+      fluidRow(
+        plotOutput("id_graf")
+      )
+    )
+  )
+))
